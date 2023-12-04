6d1d2ff97575fecb0dd668a8520d0227b6cb1619
SeungWon Kim <hesjs5@skuniv.ac.kr>
2023-12-04 16:13:58 +0900

    style: ui 변경

--- src/component/BoardCreate.js
+++ src/component/BoardCreate.js
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {boardsDomain} from "./common";
 
-export default function BoardCreate( ) {
+export default function BoardCreate() {
+
+    const navigate = useNavigate();
 
     async function create() {
         const postCreateRequest = {
@@ -9,52 +12,59 @@ export default function BoardCreate( ) {
             content: document.getElementById("content").value
         };
         const token = localStorage.getItem("token");
-        await fetch("http://localhost:8080/boards", {
+        await fetch(`${boardsDomain}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
-                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiQGIuY29tIiwiZXhwIjoxNzAwNzI4NjAzLCJpZCI6ImJAYi5jb20iLCJ1c2VybmFtZSI6ImIifQ.BUHnkr0ZkjcJzZnLy_CDaW6R9CfyOMA1lJgf5XeUrMg826ex52pYxr7W7AasuUf0frNVlLfMJX7MCAV8Rrz8gw',
+                'Authorization': 'Bearer eyJyZWdEYXRlIjoxNzAxNjU5NTg3MDYyLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJOYW1lIjoiYiIsImV4cCI6MTcwNDI1MTU4NywibWVtYmVySUQiOiJiQGIuY29tIn0.b95p5hWUG7Ct-SkDlGIyAsLbjXTOMab0aLPOE2B6eVQ',
                 'Authorization2': `Bearer ` + token,
             },
             body: JSON.stringify(postCreateRequest),
         })
             .then(response => {
                 console.log("success");
-                alert("생성됐습니다.");
-                // location.replace('/boards');
+                console.log(response);
+                goBoards();
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
+
+    function goBoards() {
+        navigate("/boards");
+    }
+
     return (
         <div className="container" style={{maxWidth: '560px'}}>
             <div className="py-5 text-center">
-                <h2>글 생성</h2>
+                <h2>글 등록</h2>
             </div>
 
-            <div className="mb-3 row">
+            <div className="row mb-3">
                 <label className="col-sm-2 col-form-label" htmlFor="title">제목</label>
                 <div className="col-sm-10">
                     <input className="form-control" id="title" name="title" placeholder="제목을 입력해주세요."
                            type="text"></input>
                 </div>
             </div>
-            <div className="mb-3 row">
+            <div className="row mb-3">
                 <label className="col-sm-2 col-form-label" htmlFor="content">내용</label>
                 <div className="col-sm-10">
-                    <textarea className="form-control" id="content" name="content" placeholder="내용을 입력해주세요."
-                              rows="10"></textarea>
+                <textarea className="form-control" id="content" name="content" placeholder="내용을 입력해주세요."
+                          rows="10"></textarea>
                 </div>
             </div>
 
-            <button className="btn btn-primary" onClick={create}>생성</button>
-            <button className="btn btn-dark">
-                <Link to="/boards" style={{textDecoration: "none", color: "white"}}>
+            <div className="d-grid gap-2">
+                <button className="btn btn-primary" onClick={create}>
+                    등록
+                </button>
+                <button className="btn btn-dark" onClick={goBoards}>
                     취소
-                </Link>
-            </button>
+                </button>
+            </div>
         </div>
     );
 }
