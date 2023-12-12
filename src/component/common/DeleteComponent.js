edb6c4c564568576a0e3f07385a823ba6cd6bb99
SeungWon Kim <hesjs5@skuniv.ac.kr>
2023-12-12 11:24:12 +0900

    refactor: 중복 코드 삭제

--- src/component/common/DeleteComponent.js
+++ src/component/common/DeleteComponent.js
@@ -5,12 +5,10 @@ import axios from "axios";
 
 export default function DeleteComponent(props) {
   const deleteRequest = async () => {
-    const token = "Bearer ".concat(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
     const headersConfig = {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiQGIuY29tIiwiZXhwIjoxNzAyMDA1MDcxLCJpZCI6ImJAYi5jb20iLCJ1c2VybmFtZSI6ImIifQ.gAB05Ljc4Vk6zkDsueKSnWzqs4sX8R18Rt53vWlM2qKmUASimNtBp_CYG5RFbvcTketqldBsfDa8GQbCwvkgdA",
-      Authorization2: token,
+      Authorization: token,
     };
     await axios
       .delete(`${props.data.requestURL}`, {
