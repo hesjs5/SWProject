0081fc01ee7287d3b9d8c85d70bf2070ccd2165a
SeungWon Kim <hesjs5@skuniv.ac.kr>
2023-12-25 17:02:16 +0900

    style: 줄바꿈 적용

--- src/common/Constant.js
+++ src/common/Constant.js
@@ -2,7 +2,7 @@ const boardsUrl = "/boards";
 const page = "page";
 const size = "size";
 
-// const baseURL = "http://54.180.113.90:8080";
-const baseURL = "http://localhost:8080";
+const baseURL = "http://54.180.113.90:8080";
+// const baseURL = "http://localhost:8080";
 
 export { boardsUrl, page, size, baseURL };
